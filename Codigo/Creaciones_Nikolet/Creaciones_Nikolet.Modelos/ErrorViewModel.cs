1e87a8ccb17cbd5740537e27c95e0e61c856b450
JuanJ189 <184328882+JuanTito-Dev@users.noreply.github.com>
2025-09-19 21:30:13 -0400

    PrimeraSubida
    Inicio

---
+++ Codigo/Creaciones_Nikolet/Creaciones_Nikolet.Modelos/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Creaciones_Nikolet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Codigo/Creaciones_Nikolet/Creaciones_Nikolet/Areas/Usuario/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Creaciones_Nikolet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Creaciones_Nikolet.Areas.Usuario.Controllers
+{
+    [Area("Usuario")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
