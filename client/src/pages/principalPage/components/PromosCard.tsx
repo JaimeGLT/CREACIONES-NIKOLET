872c2f09b5dadb918a163c02b43362e968abc562
Jaime <jaimeguzmanlt@gmail.com>
2025-09-28 21:11:54 -0400

    change name

--- client/src/pages/principalPage/components/PromosCard.tsx
+++ client/src/pages/principalPage/components/PromosCard.tsx
@@ -2,7 +2,7 @@
 
 const PromosCard = () => {
     return (
-        <div className='md:max-w-[1200px] xl:mx-auto xl:my-30 bg-white h-full w-full p-[7%] pb-[10%]'>
+        <div className='md:max-w-[1200px] xl:mx-auto xl:my-30 bg-white h-full w-full p-[7%] pb-[10%] md:py-0'>
             <div className=" h-full w-full flex justify-center items-center">
                 <img className="xl:aspect-[5/3]  object-cover w-full h-full"
                 src="/promos/promos-1.jpg" alt="Promoción destacada" />
