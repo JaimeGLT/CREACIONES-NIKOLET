80d6262e7cf3cc2ed32819c36ae2ef0e58d1269b
jaimeGLT <jaimeguzmanlt@gmail.com>
2025-11-05 06:18:43 -0400

    useEffect deleted

--- client/src/pages/SubCategory/SubCategoryPage.tsx
+++ client/src/pages/SubCategory/SubCategoryPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import SectionClothes from '../../components/SectionClothes'
 import Card from '../../components/Card'
 import { useParams } from 'react-router-dom'
